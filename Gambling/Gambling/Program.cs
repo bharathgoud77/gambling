dc818ce232fb520be7da8f16c7448685abf07e7d
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-02 18:55:42 +0530

    Gamblig UC_1 Program Done

---
+++ Gambling/Gambling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GamblerUC1
+{
+    class Gambling
+    {
+        static void Main(string[] args)
+        {
+            int Stack = 100;
+            int bet = 1;
+
+            Console.WriteLine("Stack =" + Stack);
+            Console.WriteLine("the bet =" + bet);
+
+
+
+
+        }
+    }
+}
