3753efc3a3a4e3fdd1a8fe5cc86ba7fa5bd7cecb
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-02 19:21:51 +0530

    UC_ 2 Completed

---
+++ GamblerLooseorwin/GamblerLooseorwin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GamblerLooseorwin
+{
+    public class Gambling_
+    {
+        
+        static void Main(string[] args)
+        {
+            int Gambler_win = 1;
+            Random random = new Random();
+            int Gambler = random.Next(0, 2);
+            if (Gambler == Gambler_win) 
+            {
+                Console.WriteLine(" Win");
+            }
+
+            
+            else
+            {
+                Console.WriteLine("Loose");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
