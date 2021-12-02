d476f1d53681a005db818a8e63b438f379e5f4d5
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-02 19:24:29 +0530

    Gambler May Loose Or Win

--- GamblerLooseorwin/GamblerLooseorwin/Program.cs
+++ GamblerLooseorwin/GamblerLooseorwin/Program.cs
@@ -10,6 +10,7 @@ namespace GamblerLooseorwin
             int Gambler_win = 1;
             Random random = new Random();
             int Gambler = random.Next(0, 2);
+            Console.WriteLine("invested 1$");
             if (Gambler == Gambler_win) 
             {
                 Console.WriteLine(" Win");
